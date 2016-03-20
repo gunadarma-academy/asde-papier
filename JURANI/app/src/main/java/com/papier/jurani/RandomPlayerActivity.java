a9c4630cd9f85be2898e058ba602cf35cb797f92
angellicaos <angellica.octsa@gmail.com>
2016-03-20 19:18:15 +0700

    Upload JURANI app one-third done.

---
+++ JURANI/app/src/main/java/com/papier/jurani/RandomPlayerActivity.java
@@ -0,0 +1,88 @@
+package com.papier.jurani;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+/**
+ * Created by Angellica on 3/19/2016.
+ */
+public class RandomPlayerActivity extends AppCompatActivity {
+
+    int i = 0;
+    TextView randName;
+    private String [] player;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_random);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.random_player_toolbar);
+        setSupportActionBar(toolbar);
+
+        player = getIntent().getStringArrayExtra("player");
+        randName = (TextView) findViewById(R.id.random_name);
+        //randName.setText(player[0]);
+        Runnable runName = new Runnable() {
+            @Override
+            public void run() {
+                while (i < player.length) {
+                    try {
+                        Thread.sleep(250);
+                        randName.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                randName.setText(player[i]);
+                                if (i == player.length-1) {
+                                    i = 0;
+                                } else {
+                                    i += 1;
+                                }
+                            }
+                        });
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        };
+
+        Thread runThread = new Thread(runName);
+        runThread.start();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_help) {
+            Intent goToHelp = new Intent (getApplicationContext(), HelpActivity.class);
+            startActivity(goToHelp);
+            return true;
+        } else if (id == R.id.action_developer) {
+            Intent goToDev = new Intent (getApplicationContext(), PengembangActivity.class);
+            startActivity(goToDev);
+            return true;
+        } else if (id == R.id.action_reset) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ JURANI/app/src/main/java/com/papier/jurani/SplashActivity.java
@@ -0,0 +1,38 @@
+package com.papier.jurani;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * Created by Angellica on 3/19/2016.
+ */
+public class SplashActivity extends Activity {
+
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Fullscreen
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
