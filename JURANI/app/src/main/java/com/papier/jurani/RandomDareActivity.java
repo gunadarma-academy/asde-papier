79325fa55347d4052348db7214ebafd83ad40ec3
angellicaos <angellica.octsa@gmail.com>
2016-03-23 23:51:41 +0700

    Add some features.

--- JURANI/app/src/main/java/com/papier/jurani/RandomDareActivity.java
+++ JURANI/app/src/main/java/com/papier/jurani/RandomDareActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -76,7 +78,35 @@ public class RandomDareActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_help) {
+            Intent goToHelp = new Intent (getApplicationContext(), HelpActivity.class);
+            startActivity(goToHelp);
+            return true;
+        } else if (id == R.id.action_developer) {
+            Intent goToDev = new Intent (getApplicationContext(), PengembangActivity.class);
+            startActivity(goToDev);
+            return true;
+        } else if (id == R.id.action_reset) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
